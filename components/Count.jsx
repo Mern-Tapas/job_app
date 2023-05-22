dc099e82dd555fbf00c0a0951e22279afa279551
Mern-Tapas <cyb6261452510@gmail.com>
2023-05-22 20:56:48 +0530

    udapte

--- components/Count.jsx
+++ components/Count.jsx
@@ -5,7 +5,7 @@ function Count() {
         <div className='flex justify-between '>
             <div>
                 <h2 className='text-xs text-gray-500' >Today's Orders</h2>
-                <p className=' mt-3 text-2xl md:text-xl'>$ 246</p>
+                <p className=' mt-3 text-2xl md:text-xl'>246</p>
             </div>
             <div className=''>
                 <div className="icon h-[100px] w-[100px] bg-gray-100 rounded-md">
