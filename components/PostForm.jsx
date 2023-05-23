264255d24f9425c65c5c2db838de7eedc14822aa
Mern-Tapas <cyb6261452510@gmail.com>
2023-05-23 23:11:22 +0530

    udpate

--- components/PostForm.jsx
+++ components/PostForm.jsx
@@ -11,29 +11,29 @@ function PostForm() {
             <div className='form' >
 
                 <div className="mt-3">
-                    <label for="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
                     <input type="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" required />
                 </div>
 
                 <div className="mt-3">
-                    <label for="discription" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Description</label>
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Job Description</label>
                     <textarea id="discription" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
                 </div>
 
                 <div className='grid lg:grid-cols-2 xl:grid-cols-3 gap-2'>
 
                     <div className="mt-3">
-                        <label for="start_date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date</label>
                         <input type="date" id="start_date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" required />
                     </div>
 
                     <div className="mt-3">
-                        <label for="Last_date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Date</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Date</label>
                         <input type="date" id="Last_date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" required />
                     </div>
 
                     <div className="mt-3">
-                        <label for="start_date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date</label>
                         <input type="date" id="start_date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" required />
                     </div>
 
@@ -42,12 +42,12 @@ function PostForm() {
                 <div className="grid lg:grid-cols-2 xl:grid-cols-2 gap-2">
 
                     <div className="mt-3">
-                        <label for="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
                         <input type="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" required />
                     </div>
 
                     <div className="mt-3">
-                        <label for="start_date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date</label>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date</label>
                         <input type="date" id="start_date" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title" required />
                     </div>
 
